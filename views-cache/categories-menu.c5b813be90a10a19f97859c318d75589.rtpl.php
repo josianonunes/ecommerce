207f3aaa9930dd5980f064378636cafcac099662
Josiano Nunes <josianonunes84@gmail.com>
2019-01-17 11:58:04 -0200

    Site - Categorias

---
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,5 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/2">Apple</a></li>
+<li><a href="/categories/3">Google</a></li>
+<li><a href="/categories/6">Motorola</a></li>
+<li><a href="/categories/5">Samsung</a></li>
+<li><a href="/categories/4">Sony</a></li>
