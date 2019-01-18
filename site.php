15066391e0613049f24fd8b46245e70757af0fe3
Josiano Nunes <josianonunes84@gmail.com>
2019-01-18 15:30:46 -0200

    Paginação

--- site.php
+++ site.php
@@ -15,16 +15,30 @@ $app->get('/', function() {
     ]);
 });
 
-$app->get("/categories/:idcategory", function($idcategory) {
+$app->get("/categories/:idcategory", function($idcategory){
     
-    $category = new Category();
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
-    $category->get((int) $idcategory);
-
-    $page = new Page();
-
-    $page->setTpl("category", [
-        'category' => $category->getValues(),
-        'products' => Product::checkList($category->getProducts())
-    ]);
+        $category = new Category();
+	
+        $category->get((int)$idcategory);
+	
+        $pagination = $category->getProductsPage($page);
+	
+        $pages = [];
+	
+        for ($i=1; $i <= $pagination['pages']; $i++) { 
+		array_push($pages, [
+			'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+			'page'=>$i
+		]);
+	}
+	
+        $page = new Page();
+	
+        $page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>$pagination["data"],
+		'pages'=>$pages
+	]);
 });
