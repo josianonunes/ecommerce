b1bcfb9bf32dd3979178d2e5d99ad3bd8d6ccc69
Josiano Nunes <josianonunes84@gmail.com>
2019-01-21 17:46:58 -0200

    Site Login

--- site.php
+++ site.php
@@ -4,19 +4,25 @@ use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
 use \Hcode\Model\Cart;
+use \Hcode\Model\User;
 
 $app->get('/', function() {
 
     $products = Product::listAll();
+
     $page = new Page();
     $page->setTpl("index", [
         'products' => Product::checkList($products)
     ]);
 });
+
 $app->get("/categories/:idcategory", function($idcategory) {
+
     $page = (isset($_GET['page'])) ? (int) $_GET['page'] : 1;
+
     $category = new Category();
     $category->get((int) $idcategory);
+
     $pagination = $category->getProductsPage($page);
     $pages = [];
     for ($i = 1; $i <= $pagination['pages']; $i++) {
@@ -25,6 +31,7 @@ $app->get("/categories/:idcategory", function($idcategory) {
             'page' => $i
         ]);
     }
+
     $page = new Page();
     $page->setTpl("category", [
         'category' => $category->getValues(),
@@ -32,28 +39,38 @@ $app->get("/categories/:idcategory", function($idcategory) {
         'pages' => $pages
     ]);
 });
+
 $app->get("/products/:desurl", function($desurl) {
+
     $product = new Product();
     $product->getFromURL($desurl);
+    
     $page = new Page();
     $page->setTpl("product-detail", [
         'product' => $product->getValues(),
         'categories' => $product->getCategories()
     ]);
 });
+
 $app->get("/cart", function() {
+    
     $cart = Cart::getFromSession();
+    
     $page = new Page();
     $page->setTpl("cart", [
         'cart' => $cart->getValues(),
         'products' => $cart->getProducts(),
-        'error'=>Cart::getMsgError()
+        'error' => $cart->getCartError()
     ]);
 });
+
 $app->get("/cart/:idproduct/add", function($idproduct) {
+    
     $product = new Product();
     $product->get((int) $idproduct);
+    
     $cart = Cart::getFromSession();
+    
     $qtd = (isset($_GET['qtd'])) ? (int) $_GET['qtd'] : 1;
     for ($i = 0; $i < $qtd; $i++) {
 
@@ -62,29 +79,84 @@ $app->get("/cart/:idproduct/add", function($idproduct) {
     header("Location: /cart");
     exit;
 });
+
 $app->get("/cart/:idproduct/minus", function($idproduct) {
+    
     $product = new Product();
     $product->get((int) $idproduct);
+    
     $cart = Cart::getFromSession();
     $cart->removeProduct($product);
+    
     header("Location: /cart");
     exit;
 });
+
 $app->get("/cart/:idproduct/remove", function($idproduct) {
+    
     $product = new Product();
+    
     $product->get((int) $idproduct);
+    
     $cart = Cart::getFromSession();
     $cart->removeProduct($product, true);
+    
     header("Location: /cart");
     exit;
 });
 
 $app->post("/cart/freight", function() {
-
+    
+    $nrzipcode = str_replace("-", "", $_POST['zipcode']);
+    
     $cart = Cart::getFromSession();
-
-    $cart->setFreight($_POST['zipcode']);
-
+    $cart->setFreight($nrzipcode);
+    
     header("Location: /cart");
     exit;
 });
+
+$app->get("/checkout", function() {
+    
+    User::verifyLogin(false);
+    
+    $cart = Cart::getFromSession();
+    
+    $page = new Page();
+    $page->setTpl("checkout", [
+        'cart' => $cart->getValues(),
+        'address' => []
+    ]);
+});
+
+$app->get("/login", function() {
+    
+    $page = new Page();
+    $page->setTpl("login", [
+        'loginError' => User::getError()
+    ]);
+});
+
+$app->post("/login", function() {
+    try {
+        
+        User::login($_POST["login"], $_POST["password"]);
+        
+    } catch (Exception $e) {
+        
+        User::setError($e->getMessage());
+        
+    }
+    
+    header("Location: /checkout");
+    exit;
+});
+
+$app->get("/logout", function() {
+    
+    User::logout();
+    
+    header("Location: /login");
+    exit;
+});
+?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -5,12 +5,12 @@ namespace Hcode\Model;
 use \Hcode\DB\Sql;
 use \Hcode\Model;
 use \Hcode\Mailer;
-use \Hcode\Model\User;
 
 class User extends Model {
 
     const SESSION = "User";
     const SECRET = "HcodePhp7_Secret";
+    const ERROR = "UserError";
 
     public static function getFromSession() {
         $user = new User();
@@ -40,30 +40,21 @@ class User extends Model {
     }
 
     public static function login($login, $password) {
-
         $sql = new Sql();
-
-        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+        $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b ON a.idperson = b.idperson WHERE a.deslogin = :LOGIN", array(
             ":LOGIN" => $login
         ));
-
         if (count($results) === 0) {
-            throw new \Exception("Usuário inexistente ou senha inválida");
+            throw new \Exception("Usuário inexistente ou senha inválida.");
         }
-
         $data = $results[0];
-
         if (password_verify($password, $data["despassword"]) === true) {
-
             $user = new User();
-
             $user->setData($data);
-
             $_SESSION[User::SESSION] = $user->getValues();
-
             return $user;
         } else {
-            throw new \Exception("Usuário inexistente ou senha inválida");
+            throw new \Exception("Usuário inexistente ou senha inválida.");
         }
     }
 
@@ -83,16 +74,12 @@ class User extends Model {
     }
 
     public static function listAll() {
-
         $sql = new Sql();
-
         return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
     }
 
     public function save() {
-
         $sql = new Sql();
-
         $results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
             ":desperson" => $this->getdesperson(),
             ":deslogin" => $this->getdeslogin(),
@@ -101,23 +88,19 @@ class User extends Model {
             ":nrphone" => $this->getnrphone(),
             ":inadmin" => $this->getinadmin()
         ));
-
         $this->setData($results[0]);
     }
 
     public function get($iduser) {
-
         $sql = new Sql();
-
         $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
-            "iduser" => $iduser
+            ":iduser" => $iduser
         ));
         $this->setData($results[0]);
     }
 
     public function update() {
         $sql = new Sql();
-
         $results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
             ":iduser" => $this->getiduser(),
             ":desperson" => $this->getdesperson(),
@@ -127,27 +110,24 @@ class User extends Model {
             ":nrphone" => $this->getnrphone(),
             ":inadmin" => $this->getinadmin()
         ));
-
         $this->setData($results[0]);
     }
 
     public function delete() {
-
         $sql = new Sql();
-
         $sql->query("CALL sp_users_delete(:iduser)", array(
-            ":iduser" => $this->getiduser(),
+            ":iduser" => $this->getiduser()
         ));
     }
 
-    public static function getForgot($email, $inadmin = true) {
+    public static function getForgot($email) {
         $sql = new Sql();
         $results = $sql->select("
-         SELECT *
-         FROM tb_persons a
-         INNER JOIN tb_users b USING(idperson)
-         WHERE a.desemail = :email;
-     ", array(
+			SELECT *
+			FROM tb_persons a
+			INNER JOIN tb_users b USING(idperson)
+			WHERE a.desemail = :email;
+		", array(
             ":email" => $email
         ));
         if (count($results) === 0) {
@@ -155,50 +135,40 @@ class User extends Model {
         } else {
             $data = $results[0];
             $results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
-                ":iduser" => $data['iduser'],
-                ":desip" => $_SERVER['REMOTE_ADDR']
+                ":iduser" => $data["iduser"],
+                ":desip" => $_SERVER["REMOTE_ADDR"]
             ));
             if (count($results2) === 0) {
-                throw new \Exception("Não foi possível recuperar a senha.");
+                throw new \Exception("Não foi possível recuperar a senha");
             } else {
                 $dataRecovery = $results2[0];
-                $iv = random_bytes(openssl_cipher_iv_length('aes-256-cbc'));
-                $code = openssl_encrypt($dataRecovery['idrecovery'], 'aes-256-cbc', User::SECRET, 0, $iv);
-                $result = base64_encode($iv . $code);
-                if ($inadmin === true) {
-                    $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$result";
-                } else {
-                    $link = "http://www.hcodecommerce.com.br/forgot/reset?code=$result";
-                }
-                $mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da Hcode Store", "forgot", array(
-                    "name" => $data['desperson'],
+                $code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
+                $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+                $mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Hcode Store", "forgot", array(
+                    "name" => $data["desperson"],
                     "link" => $link
                 ));
                 $mailer->send();
-                return $link;
+                return $data;
             }
         }
     }
 
-    public static function validForgotDecrypt($result) {
-        $result = base64_decode($result);
-        $code = mb_substr($result, openssl_cipher_iv_length('aes-256-cbc'), null, '8bit');
-        $iv = mb_substr($result, 0, openssl_cipher_iv_length('aes-256-cbc'), '8bit');
-
-        $idrecovery = openssl_decrypt($code, 'aes-256-cbc', User::SECRET, 0, $iv);
+    public static function validForgotDecrypt($code) {
+        $idrecovery = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, User::SECRET, base64_decode($code), MCRYPT_MODE_ECB);
         $sql = new Sql();
         $results = $sql->select("
-         SELECT *
-         FROM tb_userspasswordsrecoveries a
-         INNER JOIN tb_users b USING(iduser)
-         INNER JOIN tb_persons c USING(idperson)
-         WHERE
-         a.idrecovery = :idrecovery
-         AND
-         a.dtrecovery IS NULL
-         AND
-         DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
-     ", array(
+			SELECT * 
+			FROM tb_userspasswordsrecoveries a
+			INNER JOIN tb_users b USING(iduser)
+			INNER JOIN tb_persons c USING(idperson)
+			WHERE 
+				a.idrecovery = :idrecovery
+			    AND
+			    a.dtrecovery IS NULL
+			    AND
+			    DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
+		", array(
             ":idrecovery" => $idrecovery
         ));
         if (count($results) === 0) {
@@ -208,22 +178,33 @@ class User extends Model {
         }
     }
 
-    public static function setForgotUsed($idrecovery) {
+    public static function setFogotUsed($idrecovery) {
         $sql = new Sql();
-
         $sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
             ":idrecovery" => $idrecovery
         ));
     }
 
     public function setPassword($password) {
-
         $sql = new Sql();
-
         $sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
             ":password" => $password,
-            ":idser" => $this->getiduser()
+            ":iduser" => $this->getiduser()
         ));
+    }
+
+    public static function setError($msg) {
+        $_SESSION[User::ERROR] = $msg;
+    }
+
+    public static function getError() {
+        $msg = (isset($_SESSION[User::ERROR]) && $_SESSION[User::ERROR]) ? $_SESSION[User::ERROR] : '';
+        User::clearError();
+        return $msg;
+    }
+
+    public static function clearError() {
+        $_SESSION[User::ERROR] = NULL;
     }
 
 }
