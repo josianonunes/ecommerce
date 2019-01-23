fa60bc45091ea9856177245f083d239da0735b40
Josiano Nunes <josianonunes84@gmail.com>
2019-01-23 19:30:52 -0200

    Meus Pedidos e Resumo do Carrinho

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 use \Hcode\Model\User;
+use \Hcode\Model\Cart;
 
 function formatPrice($vlprice) {
 
@@ -22,4 +23,22 @@ function getUserName() {
 
     $user = User::getFromSession();
     return $user->getdesperson();
+}
+
+function getCartNrQtd() {
+    
+    $cart = Cart::getFromSession();
+    $totals = $cart->getProductsTotals();
+    
+    return $totals['nrqtd'];
+    
+}
+
+function getCartVlSubTotal() {
+    
+    $cart = Cart::getFromSession();
+    $totals = $cart->getProductsTotals();
+    
+    return formatPrice($totals['vlprice']);
+    
 }
